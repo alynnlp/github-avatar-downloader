59741908aa129894f67b6990030f72c6e1999cbd
Aileen Pang <aileenpang63@gmail.com>
2017-10-17 03:12:12 +0000

    create request URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,16 @@
 var request = require('request');
 
 
+
 console.log('Welcome to the Github Avatar Downloader!')
 
 var GITHUB_USER = "YOUR USERNAME HERE";
 var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
 
-GetRepoContributors(repoOwner, repoName, function(err, result) {
+getRepoContributors(repoOwner, repoName, function(err, result) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL)
+  
   console.log(result)
   if(!err){
     result.forEach(function(userData){
@@ -17,6 +21,3 @@ GetRepoContributors(repoOwner, repoName, function(err, result) {
       console.log('errors:', err)
     }
   });
-
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-console.log(requestURL)
