00e6aa223977c14f1b77e92b81e6c55604312e3a
Aileen Pang <aileenpang63@gmail.com>
2017-10-17 02:53:46 +0000

    npm init

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,22 @@
 var request = require('request');
 
+
 console.log('Welcome to the Github Avatar Downloader!')
+
+var GITHUB_USER = "YOUR USERNAME HERE";
+var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
+
+GetRepoContributors(repoOwner, repoName, function(err, result) {
+  console.log(result)
+  if(!err){
+    result.forEach(function(userData){
+      var filePath =  `${dir}${userData.login}.jpg`;
+      downloadImageByURL(userData.avatar_url, filePath);
+    })
+  } else{
+      console.log('errors:', err)
+    }
+  });
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL)
